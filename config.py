ff66f547d4bc28470f53be2a1a7532ecc4157d96
chanlikhai <155160186+chanlikhai@users.noreply.github.com>
2023-12-29 21:33:42 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cntsk5cejjyd.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chanlikhai-employee"
+customregion = "us-east-1"
+
